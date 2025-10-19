8353b39707c35627ee5fe2d8a6259ca3b489f291
yichiii <55825777+yichii@users.noreply.github.com>
2025-10-19 08:44:11 -0700

    Adding Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+  site: "https://legionwiki.info",
+  integrations: [sitemap()],
 });
